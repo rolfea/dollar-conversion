10320e5e2b6111b52007ddfdd04f72e40d85b5b6
Alex Rolfe <alex.rolfe27@gmail.com>
2019-05-17 05:56:42 -0500

    Moves spec; adds first test

--- main.spec.js
+++
@@ -1,9 +0,0 @@
-describe('testing jasmine setup', function() {
-  var a;
-
-  it('', function() {
-    a = true;
-
-    expect(a).toBe(true);
-  });
-});
---
+++ spec/main.spec.js
@@ -0,0 +1,10 @@
+describe('parse numbers 1 through 9', function() {  
+
+  it('converts $2 to "Two Dollars"', function() {
+    expect(dollarConvert('$2')).toBe('Two Dollars');
+  });
+
+  xit('converts $1 to "One Dollar" (singular)', function() {
+
+  });
+});
