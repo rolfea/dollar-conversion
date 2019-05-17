3fa685f9ccb4ce5c670eb1e01d5d85f4e442f659
Alex Rolfe <alex.rolfe27@gmail.com>
2019-05-17 06:29:00 -0500

    More test cases

--- spec/main.spec.js
+++ spec/main.spec.js
@@ -1,10 +1,39 @@
-describe('parse numbers 1 through 9', function() {  
+const dollarConvert = require('../main.js').dollarConvert;
 
-  it('converts $2 to "Two Dollars"', function() {
-    expect(dollarConvert('$2')).toBe('Two Dollars');
+describe('parse numbers 1 through 9', function() {  
+  xit('converts $1 to "One dollar" (singular)', function() {
+
   });
 
-  xit('converts $1 to "One Dollar" (singular)', function() {
+  it('converts $2 to "Two Dollars"', function() {
+    expect(dollarConvert('$2')).toBe('Two dollars');
+  });
 
+  it('converts $3 to "Three Dollars"', function() {
+    expect(dollarConvert('$2')).toBe('Two dollars');
+  });
+
+  it('converts $4 to "Four Dollars"', function() {
+    expect(dollarConvert('$2')).toBe('Two dollars');
+  });
+
+  it('converts $5 to "Five Dollars"', function() {
+    expect(dollarConvert('$2')).toBe('Two dollars');
+  });
+
+  it('converts $6 to "Six Dollars"', function() {
+    expect(dollarConvert('$2')).toBe('Two dollars');
+  });
+
+  it('converts $7 to "Seven Dollars"', function() {
+    expect(dollarConvert('$2')).toBe('Two dollars');
+  });
+
+  it('converts $8 to "Eight Dollars"', function() {
+    expect(dollarConvert('$2')).toBe('Two dollars');
+  });
+
+  it('converts $9 to "Nine Dollars"', function() {
+    expect(dollarConvert('$2')).toBe('Two dollars');
   });
 });
